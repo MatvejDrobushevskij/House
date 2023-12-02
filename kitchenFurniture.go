0c5ac7eb0e17aafb8e2d271c50bc1dd87db69727
MatvejDrobushevskij <125360757+MatvejDrobushevskij@users.noreply.github.com>
2023-12-02 13:39:51 +0300

    Create kitchenFurniture.go

---
+++ kitchenFurniture.go
@@ -0,0 +1,102 @@
+package furniture
+
+import "fmt"
+
+type Furniture struct {
+ Name     string
+ Length   int
+ Width    int
+ Height   int
+ Material string
+ Colour   string
+}
+
+type table struct {
+ Furniture Furniture
+ Shape     string
+}
+
+type chair struct {
+ Furniture Furniture
+}
+
+type wallCabinet struct {
+ Furniture Furniture
+ DoorCount int
+}
+
+type floorCabinet struct {
+ Furniture        Furniture
+ TableTopMaterial string
+}
+
+type KitchenSet struct {
+ WallCabinets  []wallCabinet
+ FloorCabinets []floorCabinet
+ Table         table
+ Chairs        []chair
+}
+
+func CreateKitchenSet() KitchenSet {
+
+ var chairsSlice []chair
+ var wallCabinetSlice []wallCabinet
+ var floorCabinetSlice []floorCabinet
+
+ for i := 0; i < 4; i++ {
+  chairsSlice = append(chairsSlice, chair{
+   Furniture: Furniture{
+    Name:     "Стул",
+    Material: "Дерево",
+    Length:   49,
+    Width:    45,
+    Height:   50,
+    Colour:   "Белый",
+   },
+  })
+  wallCabinetSlice = append(wallCabinetSlice, wallCabinet{
+   Furniture: Furniture{
+    Name:     "Подвесной шкаф",
+    Material: "Дерево",
+    Length:   50,
+    Width:    25,
+    Height:   100,
+    Colour:   "Серый",
+   },
+   DoorCount: 2,
+  })
+  floorCabinetSlice = append(floorCabinetSlice, floorCabinet{
+   Furniture: Furniture{
+    Name:     "Кухонный стол",
+    Material: "Дерево",
+    Length:   50,
+    Width:    25,
+    Height:   100,
+    Colour:   "Серый",
+   },
+   TableTopMaterial: "Мрамор",
+  })
+ }
+
+ for i := 0; i < 4; i++ {
+  fmt.Println(chairsSlice[i])
+ }
+ Table := table{
+  Furniture: Furniture{
+   Height:   100,
+   Length:   210,
+   Width:    75,
+   Material: "Стекло",
+   Colour:   "Белый",
+  },
+  Shape: "Прямоугольник",
+ }
+ kSet := KitchenSet{
+  Chairs:        chairsSlice,
+  FloorCabinets: floorCabinetSlice,
+  Table:         Table,
+  WallCabinets:  wallCabinetSlice,
+ }
+
+ return kSet
+}
